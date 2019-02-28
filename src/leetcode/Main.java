d5129d691ee80258f4b9ecd7bbb4cd650e6ccd78
lintaoko <1244275215@qq.com>
2019-02-28 13:16:59 +0800

    11111

---
+++ src/leetcode/Main.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import org.junit.Test;
+
+public class Main {
+
+        Solution solution=new Solution();
+
+        @Test
+        public void test771() {
+            //771. 宝石与石头
+            String J = "aA";
+            String S = "aAAbbbb";
+            int count = solution.numJewelsInStones(J, S);
+            System.out.println(count);
+        }
+
+        @Test
+        public void test977() {
+            //977. 有序数组的平方
+            int[] A = {-4, -1, 0, 3, 10};
+            int[] ans = solution.sortedSquares(A);
+            for (int i = 0; i < ans.length; i++) {
+                System.out.println(ans[i]);
+            }
+        }
+
+        @Test
+        public void test709(){
+        //709. 转换成小写字母
+            String str="Hello";
+            String ans=solution.toLowerCase(str);
+            System.out.println(ans);
+        }
+}
--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -1,6 +1,10 @@
 package leetcode;
 
 
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Solution {
     // 771. 宝石与石头
@@ -19,11 +23,24 @@ public class Solution {
                 }
             }
         }
-        System.out.println(count);
         return count;
     }
 
+    // 977. 有序数组的平方
+    public int[] sortedSquares(int[] A) {
+        int length=A.length;
+        int[]ans = new int[length];
+        for (int i=0; i<length;i++){
+            ans[i]=A[i]*A[i];
+        }
+        Arrays.sort(ans);
+        return  ans;
+    }
 
+    //709. 转换成小写字母
+    public String toLowerCase(String str) {
+        return  str.toLowerCase();
+    }
 
 
 }
